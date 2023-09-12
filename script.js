a287d21ca44cc286ae2971e2f914be12bc08b884
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-09-12 15:29:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 function firstNonRepeatedChar(str) {
  for(int i = 0; i < str.length; i++){
-	 if(i === 0 && str[i] !== str[i+1]){
-		 return str[i];
-	 }
-		else if(i === str.length-1 && str[i] !== str[i-1]){
-			return str[i];
-		} 
-	 else if(str[i] !== str[i-1] && str[i] !== str[i+1]){
-		 return str[i];
-	 }
+	  if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+      return str[i];
+    }
  }
 	return undefined;
 }
